eee17fcf8ddf6ae43355bb803733bb2705aa7b97
Miguel Vianna <miguelviannafeiteira@gmail.com>
2023-09-27 18:55:56 -0300

    first commit

---
+++ src/index.test.js
@@ -0,0 +1,18 @@
+import { expect, test } from "vitest"
+import { sum, div, multi, sub } from "./index"
+
+test("A soma deve retornar 5", () => {
+    expect(sum(3, 2)).toBe(5)
+})
+
+test("A subtração deve retornar 1", () => {
+    expect(sub(3, 2)).toBe(1)
+})
+
+test("A multiplicação deve retornar 6", () => {
+    expect(multi(3, 2)).toBe(6)
+})
+
+test("A divisão deve retornar 2", () => {
+    expect(div(4, 2)).toBe(2)
+})
